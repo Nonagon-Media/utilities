a8f305f0b3d6fe33de18149a8a715e63f84e9087
Troy Thompson <troycthompson@gmail.com>
2017-12-21 20:20:56 -0800

    Changed to heredoc

--- musicadd/musicadd.sh
+++ musicadd/musicadd.sh
@@ -16,6 +16,15 @@ while read -r current_song
     track_artist=$(grep -E 'Artist' /tmp/current_track| cut -d ':' -f2 | sed 's/^[[:space:]]*//')
     track_album=$(grep -E 'Album' /tmp/current_track| cut -d ':' -f2 | sed 's/^[[:space:]]*//')
     track_filepath=$current_song
-    mongo 127.0.0.1/$DB_NAME --eval "var track = {Song Title : $track_title, Artist : $track_artist, Album : $track_album, Filepath : $track_filepath};db.$DB_NAME.insert(track);"
-    #echo "Will add $track_title by $track_artist to $DB_NAME"
+    cat >> script.js <<EOF
+var track = {
+  Title : "$track_title",
+  Artist : "$track_artist",
+  Album : "track_album",
+  Filepath: "track_filepath"
+}
+db.$DB_NAME.insert(track);
+EOF
+  mongo 127.0.0.1/$DB_NAME script.js
+  rm -rf script.js
   done < /tmp/find_results
