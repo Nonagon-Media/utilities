faa193d68db9b4cdf1d1da5920ae4f1faf46c11a
Troy Thompson <troycthompson@gmail.com>
2017-12-25 08:53:02 -0800

    musicadd.sh: Added counter for stdout monitoring

--- musicadd/musicadd.sh
+++ musicadd/musicadd.sh
@@ -9,6 +9,7 @@ if [ -f /tmp/find_results ]; then
 fi
 
 find "$MUSIC_DIR" -name '*.mp3' >> /tmp/find_results
+counter=0
 while read -r current_song
   do
     id3tool "$current_song" > /tmp/current_track
@@ -16,6 +17,7 @@ while read -r current_song
     track_artist=$(grep -E 'Artist' /tmp/current_track| cut -d ':' -f2 | sed 's/^[[:space:]]*//')
     track_album=$(grep -E 'Album' /tmp/current_track| cut -d ':' -f2 | sed 's/^[[:space:]]*//')
     track_filepath=$current_song
+    echo "Adding $track_title by $track_artist"
     cat >> script.js <<EOF
 var track = {
   Title : "$track_title",
@@ -27,4 +29,6 @@ db.$DB_NAME.insert(track);
 EOF
   mongo 127.0.0.1/$DB_NAME script.js
   rm -rf script.js
+  counter=$((counter+1))
+  echo "DB_NAME should contain $counter records"
   done < /tmp/find_results
